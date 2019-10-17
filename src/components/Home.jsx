a4d1617a290059ccb7abca18ca8080d52c1d0349
nyapal <nyapal14@gmail.com>
2019-10-17 15:48:35 -0700

    centered home box

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,13 @@ import '../views/Home.css';
 const Home = () => (
 
   <div className="home">
-    <h1> Excursion App </h1>
-    <p> Group Vacations made easy. </p>
-    <button type="button">
-      <a href="/destination"> Plan A New Trip </a>
-    </button>
+    <div className="home2">
+      <h1> Excursion App </h1>
+      <p> Group Vacations made easy. </p>
+      <button type="button">
+        <a href="/destination"> Plan A New Trip </a>
+      </button>
+    </div>
   </div>
 );
 
