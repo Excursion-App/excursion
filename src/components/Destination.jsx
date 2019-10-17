ad3f54bedf57f62e92078b581a1fa8d182d125ce
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-10-17 15:47:58 -0700

    add style to button

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -12,22 +12,22 @@ const Destination = () => (
       <img src={sydney} width="360" height="240" alt="sydney opera house" className="destination-images" />
       <img src={mexico} alt="chichen itza" height="240" width="360" className="destination-images" />
     </div>
-    <div className="destination">
+    <div>
       From:
       {' '}
-      <input type="text" />
+      <input type="text" className="input" />
       {' '}
       To:
       {' '}
-      <input type="text" />
+      <input type="text" className="input" />
     </div>
 
     <div>
-      <button type="button">
+      <button type="button" className="user-flow">
         <a href="/"> Back </a>
       </button>
 
-      <button type="button">
+      <button type="button" className="user-flow">
         <a href="/travel-dates"> Next </a>
       </button>
     </div>
