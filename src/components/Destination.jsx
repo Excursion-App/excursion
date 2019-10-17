fe82be79b4fcc542a1565025942319022f6ee880
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-10-17 15:06:41 -0700

    add images to destination

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import '../views/Destination.css';
 import paris from '../images/paris.jpg';
-import egypt from '../images/pyramids.jpg';
-import sydney from '../images/sydney.jpg';
+import mexico from '../images/chichen-itza-mexico.jpg';
+import sydney from '../images/sydney-opera-house.jpg';
 
 const Destination = () => (
   <div className="destination">
     <h1> Where are you going? </h1>
     <div>
       <span>
-        <img src={paris} alt="eiffel tower" />
+        <img src={paris} alt="eiffel tower" className="destination-images" />
       </span>
       <span>
-        <img src={egypt} alt="egypt pyramids" />
+        <img src={mexico} alt="egypt pyramids" className="destination-images" />
       </span>
-      <span>
-        <img src={sydney} alt="sydney opera house" />
-      </span>
+      <img src={sydney} width="360" alt="sydney opera house" className="destination-images" />
     </div>
     <div className="destination">
       From:
