c1f5efb9221c6465849e4b8e18ff325967e5731a
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-10-17 13:59:14 -0700

    add image to destination page

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../views/Destination.css';
-import '../images/paris.jpg';
+import paris from '../images/paris.jpg';
 
 const Destination = () => (
   <div className="destination">
     <h1> Where are you going? </h1>
     <div>
       <span>
-        <img src="{paris.jpg}" alt="eiffel tower" />
+        <img src={paris} alt="eiffel tower" />
       </span>
       <span> img of greece </span>
       <span> img of sydney </span>
