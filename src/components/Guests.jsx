884216c99735aaed1038e94f036e463a40545dec
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-10-18 14:00:10 -0700

    add button

--- src/components/Guests.jsx
+++ src/components/Guests.jsx
@@ -4,6 +4,8 @@ import '../views/Guests.css';
 const Guests = () => (
   <div className="guests">
     <h1> Add your guests </h1>
+    <i className="fas fa-plus-circle" />
+    <br />
     <div>
       <button type="button" className="user-flow">
         <a href="/travel-dates"> Back </a>
