82a80cc4f23dd369b84552ddd1747c98f23cbf8f
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-03-18 13:18:52 -0400

    grammar error on indentation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,11 +15,11 @@ const Navbar = () => (
     </Link>
     <Link to="/dashboard" className="list">
       <i className="fas fa-user-alt" />
-        Sign up
+      Sign up
     </Link>
     <Link to="/dashboard" className="list">
       <i className="fas fa-user-alt" />
-        Log in
+      Log in
     </Link>
   </div>
 );
