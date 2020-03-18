dcccf1ed275c8505ac9fb6061af416ef8ea3b0fe
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-03-18 13:20:04 -0400

    Change message to user

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,7 @@ import '../views/Dashboard.css';
 const Dashboard = () => (
   <div className="dashboard">
     <h1> Dashboard </h1>
-    <p> This is where things will go. </p>
+    <p> My saved trips </p>
   </div>
 );
 
