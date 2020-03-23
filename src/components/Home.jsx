12e8a08983d0428702bbc3ecae7e495af42697d0
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-03-23 14:20:19 -0400

    accept incoming changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,21 +4,6 @@ import { Link } from 'react-router-dom';
 import Breadcrumb from './Breadcrumb';
 import Navbar from './Navbar';
 
-<<<<<<< HEAD
-class Home extends React.Component {
-  render() {
-    return (
-     <div className="home">
-       <h1> Excursions </h1>
-       <p> Group travel made easy. </p>
-       <button type="button" className="user-flow">
-        <a href="/destination"> Plan A New Trip </a>
-       </button>
-     </div>
-    )
-  }
-}
-=======
 const Home = () => (
   <>
     <Navbar />
@@ -34,6 +19,5 @@ const Home = () => (
     </div>
   </ >
 );
->>>>>>> e2cc96b5aa5757137dd287443b322daaee6009e3
 
 export default Home;
