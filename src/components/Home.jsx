f3482e6d80c5a3c92a2337077df727fd71df9149
Nyapal <nyapal14@gmail.com>
2020-10-15 10:01:28 -0700

    editing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,45 @@
-import React from 'react';
-import '../views/Home.css';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 import Navbar from './Navbar';
+import '../views/Home.css';
+import firebase from '../firebase';
+// import { Link } from 'react-router-dom';
 // import brand from '../images/excursions-2020.png';
 
-// class
-const Home = () => (
-  <div>
-    <Navbar />
-    <div className="home">
-      <div className="home2">
-        <h1 id="title"> EXCURSION </h1>
-        {/* <Link to="/" className="brand">
-          <img
-            alt="brandName"
-            className="brand"
-            src={brand}
-          />
-        </Link> */}
-        <h2 inputMode="tagline"> Group Travel Made Easy. </h2>
-        <button type="button" className="user-flow">
-          <Link to="/destination"> Plan A New Trip </Link>
-        </button>
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tripId: {}
+    };
+  }
+
+  createNewTrip() {
+    const db = firebase.firestore();
+    const newTripRef = db.collection('Trips').doc();
+    const yes = {'name': 'no'}
+    newTripRef.set(yes)
+    this.setState({ tripId: newTripRef})
+    // db.collection('Trips').add();
+    console.log('yass bby');
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="home">
+          <div className="home2">
+            <h1 id="title"> EXCURSION </h1>
+            <h2 inputMode="tagline"> Group Travel Made Easy. </h2>
+            <button type="button" className="user-flow" onClick={this.createNewTrip()}>
+              Plan A New Trip
+              {/* <Link to="/destination"> Plan A New Trip </Link> */}
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Home;
