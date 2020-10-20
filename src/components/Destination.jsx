ca9b16788332ece735059b4510e376fb3779b6f9
Nyapal <nyapal14@gmail.com>
2020-10-19 17:31:22 -0700

    fixed destination error

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -25,15 +25,16 @@ class Destination extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-
     const db = firebase.firestore();
     db.collection('Trips').add({
       destination: this.state.destination,
       origin: this.state.origin,
+    
     })
       .then((docRef) => {
         console.log(`${this.state.destination} Trip successfully created with ID ${docRef.id}`);
         this.setState({ tripId: docRef.id });
+        this.props.history.push('/travel-dates');
       })
       .catch((error) => {
         console.error('Error adding document: ', error);
@@ -50,43 +51,22 @@ class Destination extends Component {
 
 
   render() {
-    const {
-      origin, destination, cities,
-    } = this.state;
+    const { origin, destination } = this.state;
     return (
       <div>
         <Navbar />
         <Breadcrumbs />
-
         <div className="destination">
           <h1> Top Destinations </h1>
           <div>
             <img src={paris} width="240" height="160" alt="eiffel tower" className="destination-images" />
-            {/* <img src={sydney} width="240" height="160" alt="sydney opera house" className="destination-images" />
-            <img src={mexico} alt="chichen itza" width="240" height="160" className="destination-images" /> */}
+            {/* <img src={sydney} width="240" height="160" alt="sydney opera house" className="destination-images" /> */}
+            {/* <img src={mexico} alt="chichen itza" width="240" height="160" className="destination-images" />  */}
           </div>
-
           <form className="form-inline" action="/travel-dates" onSubmit={this.handleSubmit}>
             <label htmlFor="startFrom">
               From:
               <br />
-              {/* <Textbox
-                attributesInput={{
-                  id: 'startFrom',
-                  name: 'Name',
-                  type: 'text',
-                  placeholder: 'Where are you coming from?',
-                  size: '25',
-                }}
-                value={origin} // Optional.[String].Default: "".
-                onChange={this.handleStartChange}
-                onBlur={(e) => {console.log(e)}} // Optional.[Func].Default: none. In order to validate the value on blur, you MUST provide a function, even if it is an empty function. Missing this, the validation on blur will not work.
-                validationOption={{
-                  origin: 'Origin', // Optional.[String].Default: "". To display in the Error message. i.e Please enter your {name}.
-                  check: true, // Optional.[Bool].Default: true. To determine if you need to validate.
-                  required: true, // Optional.[Bool].Default: true. To determine if it is a required field.
-                }}
-              /> */}
               <input
                 type="text"
                 id="startFrom"
@@ -110,40 +90,12 @@ class Destination extends Component {
                 onChange={this.handleEndChange}
                 size="25"
               />
-              {/* <Textbox
-                attributesInput={{
-                  id: 'startFrom',
-                  name: 'Name',
-                  type: 'text',
-                  placeholder: 'Where would you like to go?',
-                  size: '25',
-                }}
-                value={destination} // Optional.[String].Default: "".
-                onChange={this.handleStartChange}
-                onBlur={(e) => {console.log(e)}} // Optional.[Func].Default: none. In order to validate the value on blur, you MUST provide a function, even if it is an empty function. Missing this, the validation on blur will not work.
-                validationOption={{
-                  destination: 'Destination',
-                  check: true,
-                  required: true,
-                }}
-              /> */}
             </label>
 
-            <button type="button" className="button">
-              <Link to="/travel-dates">
-                <i className="fas fa-search-location" />
-                Next
-              </Link>
+            <button type="submit" className="button">
+                <i className="fas fa-search-location" />Next
             </button>
           </form>
-
-          <div>
-            <div className="item">
-              {cities}
-            </div>
-            <div className="item"> </div>
-            <div className="item"> </div>
-          </div>
         </div>
       </div>
     );
