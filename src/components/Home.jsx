410dcdbe4134ac440d13104f2c16c4cea86f20d7
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-12-15 18:13:36 -0800

    change logo, drop s in Excursion

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
-import brand from '../images/excursions-2020.png';
 import '../views/Home.css';
 
 const Home = () => (
   <div>
     <Navbar />
     <div className="home">
-      <div className="home2">
-        {/* <h1 id="title"> EXCURSION </h1> */}
-        <Link to="/" className="brand">
-          <img
-            alt="brandName"
-            className="brand"
-            src={brand}
-          />
-        </Link>
+      <div className="blue-background-box">
+        <h1 id="title"> EXCURSION </h1>
         <h2 inputMode="tagline"> Group Travel Made Easy. </h2>
         <button type="button" className="user-flow">
           <Link to="/destination"> Plan A New Trip </Link>
