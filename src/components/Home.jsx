244946727fe6fcf662129dd004085b48084a9ba7
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-12-25 18:34:11 -0800

    add blue box background

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
-import brand from '../images/excursions-2020.png';
 import '../views/Home.css';
 
 const Home = () => (
   <div>
     <Navbar />
     <div className="home">
-      <div className="home2">
-        {/* <h1 id="title"> EXCURSION </h1> */}
-        <Link to="/" className="brand">
-          <img
-            alt="brandName"
-            className="brand"
-            src={brand}
-          />
-        </Link>
+      <div className="blue-background-box">
+        <h1 id="title"> EXCURSION </h1>
         <h2 inputMode="tagline"> Group Travel Made Easy. </h2>
-        <Link to="/destination">
-          <button type="button" className="user-flow">
-            Plan A New Trip
-          </button>
-        </Link>
+        <button type="button" className="user-flow">
+          <Link to="/destination"> Plan A New Trip </Link>
+        </button>
       </div>
     </div>
   </div>
