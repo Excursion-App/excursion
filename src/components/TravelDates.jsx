654fc61f6d3347a42a94cdc3fb7e20140fba825b
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-12-29 13:22:02 -0800

    add store to redux

--- src/components/TravelDates.jsx
+++ src/components/TravelDates.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Calendar from 'react-calendar';
