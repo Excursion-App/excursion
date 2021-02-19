0760d02ed263bf1b54cc891bf4e4841a0c9d6d4a
Nyapal <nyapal14@gmail.com>
2021-02-18 22:07:16 -0700

    fixing dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-// import { findLastIndex } from 'lodash';
+import React, { useState, useEffect } from 'react';
+import {
+  Tabs, Tab, TabList, TabPanel,
+} from 'react-tabs';
 import firebase from '../firebase';
 import Navbar from './Navbar';
-import VerticalTab from './VerticalTab';
 import '../views/Dashboard.css';
 
+const db = firebase.firestore();
+
 export default function Dashboard() {
+  const [trips, setList] = useState([]);
+
+  useEffect(() => {
+    db.collection('Trips').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        setList((trip) => trip.concat(data));
+      });
+    });
+  }, []);
+
   return (
     <div>
       <Navbar />
@@ -16,7 +29,23 @@ export default function Dashboard() {
           <br />
           Jaquisha!
         </h3>
-        <VerticalTab />
+        <div>
+          <Tabs>
+            <TabList>
+              { trips.map((trip) => <Tab key={trip.id}>{trip.destination}</Tab>) }
+            </TabList>
+            { trips.map((trip) => (
+              <TabPanel>
+                <div key={trip.id}>
+                  {trip.destination}
+                  Trip
+                </div>
+                <button> Invite Friends </button>
+                <button> Add Dates </button>
+              </TabPanel>
+            ))}
+          </Tabs>
+        </div>
       </div>
     </div>
   );
