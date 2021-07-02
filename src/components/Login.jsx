9bb31f2087a30deceef9c3ff24046454387872fe
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2021-07-02 14:33:24 -0700

    change Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Login = (props) => {
   const {
+    displayName,
     email,
     setEmail,
     password,
@@ -16,28 +17,75 @@ const Login = (props) => {
 
   return (
     <section className="login">
-      <div className="loginContainer">
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          autoFocus
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <p className="errorMsg">{emailError}</p>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          name=""
-          id=""
-        />
-        <p className="errorMsg">{passwordError}</p>
-        <div className="btnContainer">
-          {hasAccount ? (
+      {/* <div className="loginContainer"> */}
+      <div className="login-wrap">
+        <div className="login-html">
+          <input id="tab-1" type="radio" name="tab" className="sign-in" checked />
+          <label htmlFor="tab-1" className="tab">Log In</label>
+          <input id="tab-2" type="radio" name="tab" className="sign-up" />
+          <label htmlFor="tab-2" className="tab">Sign Up</label>
+          <div className="login-form">
+            <div className="sign-in-htm">
+              <div className="group">
+                <label htmlFor="user" className="label">Username</label>
+                <input id="user" type="text" className="input" />
+              </div>
+              <div className="group">
+                <label htmlFor="pass" className="label">Password</label>
+                <input id="pass" type="password" className="input" data-type="password" />
+              </div>
+              <div className="group">
+                <input type="submit" className="button" value="Sign In" />
+              </div>
+            </div>
+            <div className="sign-up-htm">
+            <div className="group">
+                <label htmlFor="name" className="label">Name</label>
+                <input id="name" type="text" className="input" />
+              </div>
+              <div className="group">
+                <label htmlFor="user" className="label">Email Address</label>
+                <input id="user" type="text" className="input" />
+              </div>
+              <div className="group">
+                <label htmlFor="pass" className="label">Password</label>
+                <input id="pass" type="password" className="input" data-type="password" />
+              </div>
+              <div className="group">
+                <input type="submit" className="button" value="Sign Up" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <label htmlFor="displayName">Name</label> */}
+        {/* <input
+            type="text"
+            autoFocus
+            required
+            value={displayName}
+            // onChange={(e) => setEmail(e.target.value)}
+          /> */}
+        {/* <label htmlFor="username">Email</label> */}
+        {/* <input
+            type="text"
+            autoFocus
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          /> */}
+        {/* <p className="errorMsg">{emailError}</p> */}
+        {/* <label htmlFor="password">Password</label> */}
+        {/* <input
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name=""
+            id=""
+        /> */}
+        {/* <p className="errorMsg">{passwordError}</p> */}
+        {/* <div className="btnContainer">
+            {hasAccount ? (
             <>
               <button
                 type="button"
@@ -70,8 +118,9 @@ const Login = (props) => {
               </p>
             </>
           )}
-        </div>
+          </div> */}
       </div>
+      {/* </div> */}
     </section>
   );
 };
